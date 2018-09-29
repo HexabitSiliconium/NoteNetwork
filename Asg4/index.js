0d08d812ef25c10b77c022254cee2bed4ae7d943
mwono <michaelwono@gmail.com>
2018-09-29 13:33:12 -0700

    Added part for Michael Used Morgan to generate logs. localhost:8080/logs to view

--- Asg4/index.js
+++ Asg4/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path')
 
 const app = express();
 
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs.log'), { flags: 'a' })
 
-app.use(morgan('tiny'));//Prints a log in the comman line for each HTTP request
+app.use(morgan('tiny', {stream: accessLogStream}));//Prints a log in the comman line for each HTTP request
 //:method :url :status :res[content-length] - :response-time ms
 
+try {
+	var readLog = fs.readFileSync('./logs.log');
+} catch(e) {
+	var readLog = e.stack;
+}
+
+app.get('/logs', (req, res) =>  {
+	res.send(readLog.toString());
+});
 
 app.get('/', (req,res) => {
 	res.send('PlaceHolder');
